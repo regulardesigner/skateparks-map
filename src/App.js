2ecb0e98d9117a8bb4e80598692cc0ef430ba3f6
Damien Tailhades <damien.tailhades@gmail.com>
2020-01-27 18:05:32 +0100

    STYLE & LOGIC - fullscreen maps wth POI - displaying sk8parks infos

--- src/App.js
+++ src/App.js
@@ -8,10 +8,47 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      isLoading: true,
       lat: 0,
       long: 0,
       accuracy: 0,
+      pins: [
+        {
+          id: 0,
+          lat: 49.087707,
+          long: 2.505758,
+          infos: {
+            name: 'Skateparks de marly-la-ville',
+            condition: 5
+          }
+        },
+        {
+          id: 1,
+          lat: 49.087965,
+          long: 2.559108,
+          infos: {
+            name: 'Skateparks de Saint Witz',
+            condition: 5
+          }
+        },
+        {
+          id: 2,
+          lat: 49.0330887, 
+          long: 2.4817129,
+          infos: {
+            name: 'Skateparks de Goussainville',
+            condition: 3
+          }
+        },
+        {
+          id: 3,
+          lat: 49.0511821,
+          long: 2.5099468,
+          infos: {
+            name: 'Skateparks de Louvres',
+            condition: 2
+          }
+        }
+      ]
     }
   }
   
@@ -32,7 +69,6 @@ class App extends React.Component {
           lat: crd.latitude,
           long: crd.longitude,
           accuracy: crd.accuracy,
-          isLoading: false,
         }
       )
     }
@@ -46,21 +82,30 @@ class App extends React.Component {
     const latitude = this.state.lat;
     const longitude = this.state.long;
     const accuracy = this.state.accuracy;
-    const { isLoading } = this.state;
+    const { pins } = this.state;
 
-    if(isLoading) {
-      return <div>Loading</div>
-    } else {
-      return (
-        <div className="App">
+    console.log('PINS IN STATE:', pins);
+
+    return (
+      <div className="App">
+        <header>
           <h1>Skateparks-map</h1>
-          <p>Ta latitude est : {latitude} <br/> ta longitude est : {longitude} <br/> la précision est de {accuracy} mètres.</p>
-          <Map center={[`${latitude}`, `${longitude}`]} zoom={14} width={600} height={400}>
-            <Pin anchor={[`${latitude}`, `${longitude}`]} offset={[0, 0]} />
-          </Map>
-        </div>
-      )
-    }
+        </header>
+        <Map center={[latitude, longitude]} zoom={14}>
+          {pins.map((pin, index) => (
+            <Pin
+              key={index}
+              anchor={[pin.lat, pin.long]}
+              infos={pin.infos}
+              id={index}
+            />
+          ))}
+        </Map>
+        <footer>
+            <small>lat:{latitude} long:{longitude} acc:{accuracy} meters.</small>
+        </footer>
+      </div>
+    )
   }
 }
 
--- src/Pin.js
+++ src/Pin.js
@@ -3,10 +3,32 @@ import React from 'react';
 import './App.css';
 
 const Pin = (props) => {
-  //console.log(props);
-  const { left } = props;
-  const { top } = props;
-  return <div style={{ position: 'absolute', transform: `translate(${left}px, ${top}px)`}} className="pin">here</div>
+
+  const { name, condition } = props.infos;
+  const { top, left, id } = props;
+
+  const infosCssClass = () => {
+    const show = document.querySelectorAll('.show');
+    const div = document.querySelector(`.park-${id}`);
+    div.classList.toggle('show');
+    if (show.length > 0) {
+      show.forEach(elem => elem.classList.remove('show'))
+    }
+  }
+
+  return (
+    <>
+      <div className={`infos park-${id}`}>
+        <h2>{name}</h2>
+        <p>{condition}</p>
+      </div>
+      <div
+        style={{ position: 'absolute', transform: `translate(${left}px, ${top}px)` }}
+        className="pin"
+        onClick={(e) => { infosCssClass() }}
+      >here</div>
+    </>
+  )
 }
 
 export default Pin;
