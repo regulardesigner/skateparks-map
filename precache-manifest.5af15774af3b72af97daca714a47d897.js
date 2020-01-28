113bae82a324af41f04efd09d1cd16f074a80860
Damien Tailhades <damien.tailhades@gmail.com>
2020-01-28 09:53:56 +0100

    Updates

---
+++ precache-manifest.5af15774af3b72af97daca714a47d897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1bc825d0fc32c9766701db97815510",
+    "url": "/skateparks-map/index.html"
+  },
+  {
+    "revision": "99287a535ff64df5f69f",
+    "url": "/skateparks-map/static/css/main.e7f93a47.chunk.css"
+  },
+  {
+    "revision": "d624138ffa0a2d733e49",
+    "url": "/skateparks-map/static/js/2.3d2f8617.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/skateparks-map/static/js/2.3d2f8617.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99287a535ff64df5f69f",
+    "url": "/skateparks-map/static/js/main.f819b510.chunk.js"
+  },
+  {
+    "revision": "d37c374b9bb83d388d35",
+    "url": "/skateparks-map/static/js/runtime-main.896f0f14.js"
+  }
+]);
